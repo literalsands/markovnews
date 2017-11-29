a146afd72183fed024e5c036a9da0eca4310a62c
joel-g <joelaguerra1@gmail.com>
2017-11-29 14:59:42 -0800

    added more news

--- get_news.py
+++ get_news.py
@@ -4,17 +4,20 @@ import re
 
 def article_from_html(text, article="div.story-body__inner"):
     soup = BeautifulSoup(text, "html.parser")
-    article = soup.select(article)[0]
-    for script_tag in soup.select('script'):
-        script_tag.decompose()
-    for script_tag in soup.select('img'):
-        script_tag.decompose()
-    for script_tag in soup.select('figure'):
-        script_tag.decompose()
-    text = article.get_text()
-    text = re.sub(r'[^a-zA-Z0-9,. ]', ' ', text)
-    text = re.sub(r'\s+', ' ', text)
-    return text
+    try:
+        article = soup.select(article)[0]
+        for script_tag in soup.select('script'):
+            script_tag.decompose()
+        for script_tag in soup.select('img'):
+            script_tag.decompose()
+        for script_tag in soup.select('figure'):
+            script_tag.decompose()
+        text = article.get_text()
+        text = re.sub(r'[^a-zA-Z0-9,. ]', ' ', text)
+        text = re.sub(r'\s+', ' ', text)
+        return text
+    except:
+        return ""
 
 def get_news(site):
     return article_from_html(requests.get(site).text)
--- markovnews.py
+++ markovnews.py
@@ -1,4 +1,5 @@
 import requests, sys, tweepy, re, time, markovify
+from get_news import get_news
 
 config = open('config.ini','r')
 tokens = config.readlines()
@@ -16,19 +17,30 @@ api = tweepy.API(auth)
 
 # api.update_status("test")
 
-def get_news_urls():
-    url = ('https://newsapi.org/v2/top-headlines?'
-       'sources=bbc-news&'
+def get_news_object(news_source):
+        url = ('https://newsapi.org/v2/top-headlines?'
+       'sources=' + news_source + '&'
        'apiKey=' + NEWS_API_KEY)
-    response = requests.get(url)
+        response = requests.get(url)
+        return response
+
+       
+
+def get_news_urls(response):
     url_list = []
     for article in response.json()['articles']:
         url_list.append(article['url'])
     return url_list
 
+def get_news_headlines(response):
+    headlines = []
+    for article in response.json()['articles']:
+        headlines.append(article['title'] + " " + article["description"])
+    return headlines
+
 
 def markov_the_news(text):
-    model = markovify.NewlineText(text)
+    model = markovify.Text(text)
     return model.make_sentence()
 
 # while True:
@@ -38,3 +50,17 @@ def markov_the_news(text):
 
 # for x in response.json()['articles']:
 #     print(x['urlToImage'])
+
+
+bbc_response = get_news_object('bbc-news')
+nbc_response = get_news_object('engadget')
+msnbc_response = get_news_object('nbc-news')
+# url_list = get_news_urls(response)
+bbc_headlines = get_news_headlines(bbc_response)
+nbc_headlines = get_news_headlines(nbc_response)
+msnbc_headlines = get_news_headlines(msnbc_response)
+
+# article = get_news(url_list[0])
+
+for x in range(10):
+    print(markov_the_news(' '.join(bbc_headlines + nbc_headlines + msnbc_headlines)))
