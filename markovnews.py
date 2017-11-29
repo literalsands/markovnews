9f682dce10688c290dd5fa1effbd094e195551a3
joel-g <joelaguerra1@gmail.com>
2017-11-29 13:16:57 -0800

    adds news api and markov functions

--- markovnews.py
+++ markovnews.py
@@ -8,7 +8,25 @@ CONSUMER_KEY = tokens[0].rstrip()
 CONSUMER_SECRET = tokens[1].rstrip()
 ACCESS_KEY = tokens[2].rstrip()
 ACCESS_SECRET = tokens[3].rstrip()
+NEWS_API_KEY = tokens[4].strip()
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+# api.update_status("test")
+
+def get_news_urls():
+    url = ('https://newsapi.org/v2/top-headlines?'
+       'sources=bbc-news&'
+       'apiKey=' + NEWS_API_KEY)
+    response = requests.get(url)
+    url_list = []
+    for article in response.json()['articles']:
+        url_list.append(article['url'])
+    return url_list
+
+
+def markov_the_news(text):
+    model = markovify.NewlineText(text)
+    return model.make_sentence()
